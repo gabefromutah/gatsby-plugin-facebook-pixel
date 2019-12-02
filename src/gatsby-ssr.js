08a40184a8af9b63816f79e54f17c12f80881ed0
Nathan <bre7@users.noreply.github.com>
2019-12-01 21:32:01 -0300

    Use setPostBodyComponents & <noscript>

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+exports.onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production` && pluginOptions.pixelId) {
-    return setHeadComponents([
+    return setPostBodyComponents([
       <script
         key={`gatsby-plugin-facebook-pixel`}
         dangerouslySetInnerHTML={{
           __html: `
-  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-  document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '${pluginOptions.pixelId}'); // Insert your pixel ID here.
-  fbq('track', 'PageView');
-      `,
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pluginOptions.pixelId}'); // Insert your pixel ID here.
+            fbq('track', 'PageView');
+          `,
         }}
       />,
-    ]);
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${pluginOptions.pixelId}&ev=PageView&noscript=1`}
+        />
+      </noscript>,
+    ])
   }
-};
+}
